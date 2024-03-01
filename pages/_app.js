23c72b197de8b7818ab0c4c9e64c7dad659f27f6
Oleh Bilostotskiy <89212792+OlehWhite@users.noreply.github.com>
2024-03-01 21:40:23 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/global.css";
 
+
 const theme = createTheme({});
 
 const MyApp = ({ Component, pageProps }) => (
